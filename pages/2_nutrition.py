27d237b06e456696ef748b28ea23e416bcbfd427
LaxmiVatsalyaDaita <87368740+LaxmiVatsalyaDaita@users.noreply.github.com>
2024-10-17 10:37:36 -0400

    Update 2_nutrition.py

--- pages/2_nutrition.py
+++ pages/2_nutrition.py
@@ -14,7 +14,7 @@ st.set_page_config(page_title="Nutrition EDA Dashboard", page_icon="🍎", layou
 # Load the data
 @st.cache_data
 def load_data():
-    url = "https://github.com/LaxmiVatsalyaDaita/CMSE830/blob/main/pages/nutrition_cleaned.csv"
+    url = "https://raw.github.com/LaxmiVatsalyaDaita/CMSE830/blob/main/pages/nutrition_cleaned.csv"
     #data = pd.read_csv('nutrition_cleaned.csv')
     data = pd.read_csv(url, delimiter=",")
     return data
